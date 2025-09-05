c666d393f49b414358714c00f39016c148262642
gaurav <gautamgaurav849@gmail.com>
2025-09-05 18:49:49 +0530

    Navbar Done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+            <li><a href="/"></a>Home</li>
+            <li><a href="/"></a>Volume</li>
+            <li><a href="/"></a>Characters</li>
+            <li><a href="/"></a>FeedBack</li>
+            <li><a href="/"></a>About Writter</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
