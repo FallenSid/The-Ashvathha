abee5804ec2d372a6bcb0ae578c3df4359ba7979
gaurav <gautamgaurav849@gmail.com>
2025-09-09 22:19:54 +0530

    done

--- src/Pages/Volume.jsx
+++ src/Pages/Volume.jsx
@@ -5,28 +5,24 @@ const Volume = () => {
     <>
       <Navbar />
       <div className="container">
-        <img src="Ashvathha\src\assets\image\volbg.jpg" alt="Volume-Background" />
-        {/* <div className="article">
+        <img id="volbg" src="src/assets/image/volbg.jpg" alt="" />
+        <div className="article">
           Nothing's new
-          {/* this is side-bar */}
-        {/* </div>
+           {/* this is side-bar */}
+        </div>
         <div className="main">
-          main area where link to volume will be attached
+          {/* main area where link to volume will be attached */}
           <h1>Volume</h1>
           <ul>
             <li><a href="#">Volume 1</a></li>
             <li><a href="#">Volume 2</a></li>
             <li><a href="#">Volume 3</a></li>
           </ul>
-          SORRY VOLUME NOT AVAILABLE. . .
-          <div className="footer">
-            <hr />
-          </div>
         </div>
         <div className="section">
           Nothing to See Here
-          another sidebar for additional stuffs and promotions
-        </div> */} 
+          {/* another sidebar for additional stuffs and promotions */}
+        </div> 
       </div>
     </>
   );
