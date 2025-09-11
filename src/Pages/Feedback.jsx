20bc4dcc27ac17140509265eae389d701e414e41
gaurav <gautamgaurav849@gmail.com>
2025-09-11 21:24:59 +0530

    Feedback Page Added

--- src/Pages/Feedback.jsx
+++ src/Pages/Feedback.jsx
@@ -1,12 +1,54 @@
-import Navbar from "../components/Navbar"
+import Navbar from "../components/Navbar";
+import "./feedback.css";
 
 const Feedback = () => {
   return (
-    <div>
-      <Navbar/>
-      Hey i am Feedback Page
-    </div>
-  )
-}
+    <div classname="container">
+      <Navbar />
 
-export default Feedback
+       <img
+        src="src\assets\image\download (31).jpeg.jpg"
+        alt="Feedback-background"
+        id="feedback-bg"
+       />
+     <div className="main">
+
+
+      <div className="char-img">
+        <img
+          src="src\assets\image\char\this guy-Photoroom.png"
+          alt="Monster-image"
+          id="monster-img"
+          />
+      </div>
+
+      <div className="content">
+        <h1>Tell Us What's Inside you</h1>
+
+        <form action="#">
+          <div className="name-input">
+            <label htmlFor="name">Name:</label>
+            <input type="text" placeholder="Human..." id="name" />
+          </div>
+
+          <div className="textarea">
+            <label htmlFor="feedback">FeedBack:</label>
+            <textarea
+              name="textarea"
+              id="feedback"
+              placeholder="I am Filthy Monkey"
+              ></textarea>
+          </div>
+
+          <br />
+
+          <input type="submit" value="Submit" />
+          <input type="reset" value="Reset" />
+        </form>
+      </div>
+     </div>
+    </div>
+  );
+};
+
+export default Feedback;
